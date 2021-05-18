101a30d3b2799047e02f0cd23a16fd50db6d68ef
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2021-05-18 18:13:36 +0000

    Major Update

--- networks/da.py
+++ networks/da.py
@@ -6,4 +6,13 @@ import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 
 from neuralnets.networks.unet import UNet2D, UNetDecoder2D, UNetEncoder2D
-from neuraln
+from neuralnets.networks.cnn import CNN2D
+from neuralnets.util.losses import CrossEntropyLoss, L2Loss
+from neuralnets.data.datasets import LabeledVolumeDataset
+
+from networks.base import UNetDA2D, UNetDA2DClassifier, data_from_range, feature_regularization_loss, ReverseLayerF
+
+
+class UNetMMD2D(UNetDA2D):
+
+    def __init__(self, input_shape=(1, 256, 
