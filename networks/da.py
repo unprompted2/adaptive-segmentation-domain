4cfcdbe20d95e824bc0a054773b6def7a6a4a70a
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2021-05-20 07:12:32 +0000

    robo.phar published

--- networks/da.py
+++ networks/da.py
@@ -15,4 +15,7 @@ from networks.base import UNetDA2D, UNetDA2DClassifier, data_from_range, feature
 
 class UNetMMD2D(UNetDA2D):
 
-    def __init__(self, input_shape=(1, 256, 
+    def __init__(self, input_shape=(1, 256, 256), in_channels=1, coi=(0, 1), feature_maps=64, levels=4,
+                 skip_connections=True, residual_connections=False, norm='instance', activation='relu', dropout_enc=0.0,
+                 dropout_dec=0.0, loss_fn='ce', lr=1e-3, lambda_mmd=0):
+        super().__init__(input_shape=input_shape, in_channels=in_channels
