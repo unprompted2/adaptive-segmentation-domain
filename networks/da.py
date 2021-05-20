b7098e017422f9e39383b3eca4ba51a44053e523
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2021-05-20 08:49:28 +0000

    lib/checksum.c: fix carry in csum_tcpudp_nofold

--- networks/da.py
+++ networks/da.py
@@ -18,4 +18,9 @@ class UNetMMD2D(UNetDA2D):
     def __init__(self, input_shape=(1, 256, 256), in_channels=1, coi=(0, 1), feature_maps=64, levels=4,
                  skip_connections=True, residual_connections=False, norm='instance', activation='relu', dropout_enc=0.0,
                  dropout_dec=0.0, loss_fn='ce', lr=1e-3, lambda_mmd=0):
-        super().__init__(input_shape=input_shape, in_channels=in_channels
+        super().__init__(input_shape=input_shape, in_channels=in_channels, coi=coi, feature_maps=feature_maps,
+                         levels=levels, skip_connections=skip_connections, residual_connections=residual_connections,
+                         norm=norm, activation=activation, dropout_enc=dropout_enc, dropout_dec=dropout_dec,
+                         loss_fn=loss_fn, lr=lr, return_features=True)
+
+        self.lambda_mmd = lamb
