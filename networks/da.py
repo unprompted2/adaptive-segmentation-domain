e33ae9c09caa803c5079020c017bd79116d1b34a
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2021-05-24 22:49:13 +0000

    Added semicolon

--- networks/da.py
+++ networks/da.py
@@ -44,4 +44,10 @@ class UNetMMD2D(UNetDA2D):
         loss = loss_src + loss_tar + self.lambda_mmd * loss_mmd
 
         # compute iou
-        y_src_pred = torch.softmax(y_src_pred, dim=1
+        y_src_pred = torch.softmax(y_src_pred, dim=1)
+        y_tar_pred = torch.softmax(y_tar_pred, dim=1)
+        mIoU_src = self._mIoU(y_src_pred, y_src)
+        mIoU_tar = self._mIoU(y_tar_pred, y_tar) if tar_labels_available else -1
+        self.log('train/mIoU_src', mIoU_src, prog_bar=True)
+        self.log('train/loss_src', loss_src)
+        self.lo
