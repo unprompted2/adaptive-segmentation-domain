4cbfd63e0dfcac5383b6a08db87041673cf12628
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2021-05-26 07:38:56 +0000

    removed backup files

--- networks/da.py
+++ networks/da.py
@@ -50,4 +50,13 @@ class UNetMMD2D(UNetDA2D):
         mIoU_tar = self._mIoU(y_tar_pred, y_tar) if tar_labels_available else -1
         self.log('train/mIoU_src', mIoU_src, prog_bar=True)
         self.log('train/loss_src', loss_src)
-        self.lo
+        self.log('train/loss_mmd', loss_mmd, prog_bar=True)
+        self.log('train/loss', loss)
+        u = y_tar.unique()
+        if u.numel() != 1 or int(u) != 255:
+            self.log('train/mIoU_tar', mIoU_tar, prog_bar=True)
+            self.log('train/loss_tar', loss_tar)
+
+        # log images
+        if batch_idx == self.train_batch_id:
+ 
