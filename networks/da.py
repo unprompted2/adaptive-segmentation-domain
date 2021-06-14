0cb42a83eab15e17c9c4d1716d303ffaad99520e
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2021-06-14 22:37:19 +0000

    cosmetic

--- networks/da.py
+++ networks/da.py
@@ -198,4 +198,12 @@ class UNetDAT2D(UNetDA2D):
         loss_src = self.loss_fn(y_src_pred, y_src[:, 0, ...])
         loss_tar = self.loss_fn(y_tar_pred, y_tar[:, 0, ...]) if tar_labels_available else 0
         loss_dat = self.loss_ce(dom_pred, dom_labels)
-        loss = loss_src + loss_tar + self.lambda_d
+        loss = loss_src + loss_tar + self.lambda_dat * loss_dat
+
+        # compute iou
+        y_src_pred = torch.softmax(y_src_pred, dim=1)
+        y_tar_pred = torch.softmax(y_tar_pred, dim=1)
+        mIoU_src = self._mIoU(y_src_pred, y_src)
+        mIoU_tar = self._mIoU(y_tar_pred, y_tar) if tar_labels_available else -1
+        self.log('train/mIoU_src', mIoU_src, prog_bar=True)
+        self.log('train/lo
