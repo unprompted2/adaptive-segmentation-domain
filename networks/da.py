25651b13fb4cc6b61d314e664536c49012880af0
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2021-06-23 16:13:42 +0000

    ath9k_htc: properly set MAC address and BSSID mask

--- networks/da.py
+++ networks/da.py
@@ -243,4 +243,11 @@ class UNetDAT2D(UNetDA2D):
         loss = loss_src + loss_tar + self.lambda_dat * loss_dat
 
         # compute iou
-        y_src_pred = torch.softma
+        y_src_pred = torch.softmax(y_src_pred, dim=1)
+        y_tar_pred = torch.softmax(y_tar_pred, dim=1)
+        mIoU_src = self._mIoU(y_src_pred, y_src)
+        mIoU_tar = self._mIoU(y_tar_pred, y_tar) if tar_labels_available else -1
+        self.log('val/mIoU_src', mIoU_src)
+        self.log('val/mIoU_tar', mIoU_tar, prog_bar=True)
+        self.log('val/loss_src', loss_src)
+     
