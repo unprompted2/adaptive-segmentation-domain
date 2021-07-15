4f9e2fedc6ff26d96a6f213d6b5d3a620288e8c2
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2021-07-15 07:16:20 +0000

    x86/platform: Fix Geode LX timekeeping in the generic x86 build

--- networks/da.py
+++ networks/da.py
@@ -364,4 +364,14 @@ class YNet2D(UNetDA2D):
         mIoU_src = self._mIoU(y_src_pred, y_src)
         mIoU_tar = self._mIoU(y_tar_pred, y_tar) if tar_labels_available else -1
         self.log('train/mIoU_src', mIoU_src, prog_bar=True)
-        self.log('train/loss_src', loss_src
+        self.log('train/loss_src', loss_src)
+        self.log('train/loss_rec', loss_rec, prog_bar=True)
+        self.log('train/loss', loss)
+        u = y_tar.unique()
+        if u.numel() != 1 or int(u) != 255:
+            self.log('train/mIoU_tar', mIoU_tar, prog_bar=True)
+            self.log('train/loss_tar', loss_tar)
+
+        # log images
+        if batch_idx == self.train_batch_id:
+            self._log_pred
