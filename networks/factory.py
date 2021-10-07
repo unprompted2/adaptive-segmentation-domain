c3d18ea37a33afafa9a9ff79da776db7581302d4
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2021-10-07 22:49:21 +0000

    Add space

--- networks/factory.py
+++ networks/factory.py
@@ -1,3 +1,13 @@
 
 from networks.no_da import UNetNoDA2DClassifier
-from networks.d
+from networks.da import *
+
+from neuralnets.util.io import print_frm
+
+
+def generate_model(name, params):
+
+    if name == 'u-net' or name == 'no-da':
+        net = UNetDA2D(in_channels=params['in_channels'], feature_maps=params['fm'], levels=params['levels'],
+                       dropout_enc=params['dropout'], dropout_dec=params['dropout'], norm=params['norm'],
+      
