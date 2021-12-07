f2b9434a895b96ac0dad81aafd9dac81198c3176
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2021-12-07 19:14:32 +0000

    add version info

--- networks/no_da.py
+++ networks/no_da.py
@@ -1,4 +1,18 @@
 import torch
 import pytorch_lightning as pl
 
-from torch.utils.data impo
+from torch.utils.data import DataLoader
+from networks.base import UNetDA2D, UNetDA2DClassifier, data_from_range
+
+from neuralnets.data.datasets import LabeledVolumeDataset
+
+
+class UNetNoDA2D(UNetDA2D):
+
+    def training_step(self, batch, batch_idx):
+
+        # get data
+        x, y = batch
+        x_src, x_tar = x
+        y_src, y_tar = y
+        tar_
