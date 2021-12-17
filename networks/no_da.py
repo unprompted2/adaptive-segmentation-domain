54a7ae02cd628bd6c90cf91573b0a8aaf8fc276a
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2021-12-17 13:00:59 +0000

    Update year in LICENSE

--- networks/no_da.py
+++ networks/no_da.py
@@ -64,4 +64,10 @@ class UNetNoDA2D(UNetDA2D):
         loss = loss_src + loss_tar
 
         # compute iou
-        y
+        y_src_pred = torch.softmax(y_src_pred, dim=1)
+        y_tar_pred = torch.softmax(y_tar_pred, dim=1)
+        mIoU_src = self._mIoU(y_src_pred, y_src)
+        mIoU_tar = self._mIoU(y_tar_pred, y_tar) if tar_labels_available else -1
+        self.log('val/mIoU_src', mIoU_src, prog_bar=True)
+        self.log('val/mIoU_tar', mIoU_tar, prog_bar=True)
+        self.log('v
