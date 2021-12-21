163d48ba29717934dafd788e4f7aff3c932a6f52
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2021-12-21 21:20:18 +0000

    Fix bugs.

--- networks/no_da.py
+++ networks/no_da.py
@@ -90,4 +90,8 @@ class UNetNoDA2DClassifier(UNetDA2DClassifier):
 
         # initialize model and trainer
         self.model = UNetNoDA2D(in_channels=self.in_channels, feature_maps=self.feature_maps, levels=self.levels,
-                                dropout_enc=self.dropout, dropout_dec=s
+                                dropout_enc=self.dropout, dropout_dec=self.dropout, norm=self.norm,
+                                activation=self.activation, coi=self.coi, loss_fn=self.loss_fn)
+        self.trainer = pl.Trainer(max_epochs=int(self.epochs), gpus=self.gpus, accelerator=self.accelerator,
+                                  default_root_dir=self.log_dir, flush_logs_every_n_steps=self.log_freq,
+  
