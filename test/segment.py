56e61d3da29fff7dd02593af0a8e41b1613921f4
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2022-01-03 22:13:04 +0000

    Update ref

--- test/segment.py
+++ test/segment.py
@@ -15,4 +15,16 @@ from neuralnets.util.tools import set_seed
 from neuralnets.util.validation import segment_read, segment_ram
 
 from util.tools import parse_params, process_seconds
-from net
+from networks.factory import generate_model
+
+from multiprocessing import freeze_support
+
+if __name__ == '__main__':
+    freeze_support()
+
+    """
+        Parse all the arguments
+    """
+    print_frm('Parsing arguments')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", "-c", help="Path to the network configuration file", type=st
