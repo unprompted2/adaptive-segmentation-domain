f97dff4c93c48508d56c0eddec8326b98de8f6dd
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2022-01-03 18:16:33 +0000

    Webhook test

--- test/segment.py
+++ test/segment.py
@@ -1,2 +1,18 @@
 """
-    This is a script that illustrates segmentation of a dataset with a
+    This is a script that illustrates segmentation of a dataset with a particular model
+"""
+import torch
+
+"""
+    Necessary libraries
+"""
+import argparse
+import yaml
+import time
+
+from neuralnets.util.io import print_frm, read_pngseq
+from neuralnets.util.tools import set_seed
+from neuralnets.util.validation import segment_read, segment_ram
+
+from util.tools import parse_params, process_seconds
+from net
