008642c02e3c5853f55f060309c582661a6d372c
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2022-03-29 12:12:31 +0000

    v16

--- util/base_ssda.sh
+++ util/base_ssda.sh
@@ -1,1 +1,14 @@
-#!/
+#!/bin/bash -l
+#PBS -l nodes=1:ppn=8:gpus=1
+#PBS -l mem=32gb
+#PBS -l walltime=28:00:00
+module purge
+module load PyTorch
+module unload PyTorch
+
+# define context variables
+PYTHON_EXE=python
+
+# define environment variables
+PROJECT_DIR=$HOME/research/domain-adaptive-segmentation
+export PYTHONPATH=$HOME/research/neuralnets:$HOME/
