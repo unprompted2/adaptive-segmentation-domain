31c07e85114df9d3e4227fa1888cb35f6ec82acd
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2022-04-10 08:21:03 +0000

    update scripts

--- util/build_config_supervised.py
+++ util/build_config_supervised.py
@@ -1,3 +1,22 @@
 
 import os
-import
+import argparse
+import yaml
+from yaml.loader import SafeLoader
+
+# domains
+EPFL = 'EPFL'
+UROCELL = 'UroCell'
+PO936Q = 'po936q'
+MITOEM_H = 'MitoEM-H'
+MITOEM_R = 'MitoEM-R'
+MIRA = 'MiRA'
+KASTHURI = 'Kasthuri'
+VNC = 'VNC'
+EMBL_HELA = 'EMBL'
+VIB_EVHELA = 'evhela'
+
+
+def _default_params():
+
+    params = {'train_val_test_split': {}, 'split_orientation': {}, 'input_si
