47b9fc8eee1202525f8794d0ca5b9b37cbd4b839
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2022-06-17 17:07:59 +0000

    argh

--- util/build_script_da.py
+++ util/build_script_da.py
@@ -11,4 +11,19 @@ PO936Q = 'po936q'
 MITOEM_H = 'MitoEM-H'
 MITOEM_R = 'MitoEM-R'
 MIRA = 'MiRA'
-KA
+KASTHURI = 'Kasthuri'
+VNC = 'VNC'
+EMBL_HELA = 'EMBL'
+VIB_EVHELA = 'evhela'
+DOMAINS = [VIB_EVHELA, VNC, MITOEM_H, EPFL, KASTHURI]
+SRC_DOMAINS = '"' + '" "'.join(DOMAINS) + '"'
+
+# methods
+NO_DA = 'no-da'
+MMD = 'mmd'
+DAT = 'dat'
+YNET = 'ynet'
+UNET_TS = 'unet-ts'
+METHODS = [NO_DA, MMD, DAT, YNET, UNET_TS]
+
+# available labe
