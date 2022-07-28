4d31051d87071bc8a6e377e5d7abc043f718a986
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2022-07-28 09:16:01 +0000

    cmpitg

--- util/build_script_da.py
+++ util/build_script_da.py
@@ -34,4 +34,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--base_file", "-b", help="Path to the base script", required=True, type=str)
 parser.add_argument("--target_dir", "-t", help="Path to the directory where the scripts will be saved", required=True,
                     type=str)
-parser.add
+parser.add_argument("--coi", "-c", help="Class of interest", type=int, default=1)
+args = parser.parse_args()
+
+# load the base script
+mkdir(args.target_dir)
+with open(args.base_file, 'r') as f:
+    lines = f.readlines()
+    for method in METHODS:
+        for i, al in enumerate(AVAILABLE_LABELS):
+            for src_domain in DOMAINS:
+                for tar_domain in DOMAINS:
