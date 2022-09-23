1e6611376aa458798e5562464349139993b11b9b
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2022-09-23 11:30:12 +0000

    .travis.yml: update to match main repo

--- util/build_script_uda.py
+++ util/build_script_uda.py
@@ -26,3 +26,37 @@ DAT = 'dat'
 YNET = 'ynet'
 UNET_TS = 'unet-ts'
 METHODS = [NO_DA, MMD, DAT, YNET, UNET_TS]
+
+# available labels
+AVAILABLE_LABELS = [0.0]
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--base_file", "-b", help="Path to the base script", required=True, type=str)
+parser.add_argument("--target_dir", "-t", help="Path to the directory where the scripts will be saved", required=True,
+                    type=str)
+parser.add_argument("--coi", "-c", help="Class of interest", type=int, default=1)
+args = parser.parse_args()
+
+# load the base script
+mkdir(args.target_dir)
+with open(args.base_file, 'r') as f:
+    lines = f.readlines()
+    for method in METHODS:
+        for i, al in enumerate(AVAILABLE_LABELS):
+            for src_domain in DOMAINS:
+                for tar_domain in DOMAINS:
+                    if src_domain != tar_domain:
+
+                        lines_ = []
+                        for line in lines:
+                            line = line.replace('<METHOD>', method)
+                            line = line.replace('<COI>', str(args.coi))
+                            line = line.replace('<AVAILABLE_LABELS>', str(al))
+                            line = line.replace('<SRC_DOMAINS>', '"' + src_domain + '"')
+                            line = line.replace('<TAR_DOMAINS>', '"' + tar_domain + '"')
+                            lines_.append(line)
+
+                        with open(os.path.join(args.target_dir, 'run_%s_%d_%s2%s.sh' % (method, i, src_domain, tar_domain)), 'w') as f:
+                            for line in lines_:
+                                f.write(line)
---
+++ util/parse_logs_ssda.py
@@ -0,0 +1,13 @@
+
+import os
+
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 16})
+
+
+DPI = 100
+
+
+def _symmetry(A, 
