f6ca4b24dbde6b2c18bcb17fdabbd1150596b915
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2022-09-30 10:40:16 +0000

    black

--- util/parse_logs_ssda.py
+++ util/parse_logs_ssda.py
@@ -10,4 +10,21 @@ plt.rcParams.update({'font.size': 16})
 DPI = 100
 
 
-def _symmetry(A, 
+def _symmetry(A, ord='fro'):
+
+    A_sym = 0.5 * (A + A.T)
+    A_anti = 0.5 * (A - A.T)
+
+    s_sym = np.linalg.norm(A_sym, ord=ord)
+    s_anti = np.linalg.norm(A_anti, ord=ord)
+
+    s = (s_sym - s_anti) / (s_sym + s_anti)
+
+    return s
+
+
+def _get_filename(method, frac, src, tar):
+    return method + '-' + str(frac) + '-' + src + '2' + tar + '.logs'
+
+
+def _parse_file_con
