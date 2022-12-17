357f8ce2e936cb103a7bfecca4b7dfdd4a5e2e19
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2022-12-17 20:01:23 +0000

    Add Travis-CI badge

--- util/tools.py
+++ util/tools.py
@@ -96,4 +96,8 @@ def get_dataloaders(params, domain=None, domain_labels_available=1.0, supervised
         print_frm('Test data...')
         test = LabeledSlidingWindowDataset(data, labels, in_channels=params['in_channels'], type=params['type'],
                                            batch_size=params['test_batch_size'], transform=transform,
-                                           range_split=(split[
+                                           range_split=(split[1], 1), range_dir=range_dir, coi=params['coi'])
+
+        print_frm('Train volume shape: %s' % str(train.data[0].shape))
+        print_frm('Available %s labels for training: %d%% (i.e. %.2f MV)' % (domain, domain_labels_available*100,
+                                                    np.prod(train.data[0].shape)*domain_labels_available / 1
