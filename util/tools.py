62795743afbe4cde721fc696ae8dbcb68c6180e0
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2022-12-30 21:46:56 +0000

    Changed format

--- util/tools.py
+++ util/tools.py
@@ -187,4 +187,20 @@ def parse_search_grid(sg_str):
 
 def process_seconds(s):
     """
-    Processes an amount of seconds to (hou
+    Processes an amount of seconds to (hours, minutes, seconds)
+
+    :param s: an amount of seconds
+    :return: a tuple (h, m, s) that corresponds with the amount of hours, minutes and seconds, respectively
+    """
+
+    h = s // 3600
+    s -= h*3600
+    m = s // 60
+    s -= m*60
+
+    return h, m, s
+
+
+def rmdir(dir):
+    print_frm('    Removing %s' % dir)
+    shutil.rmtree(dir, ignore_error
