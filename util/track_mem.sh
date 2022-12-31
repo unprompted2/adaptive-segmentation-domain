bbcfd90135a26005173e11da58b46b0eab6a9380
unprompted2 <184451572+unprompted2@users.noreply.github.com>
2022-12-31 17:54:03 +0000

    readme renamed

--- util/track_mem.sh
+++ util/track_mem.sh
@@ -4,4 +4,23 @@
 # required parameters: 
 #       param #1: PID of the process to track
 
-pid=
+pid=$1
+
+max_mem=0
+max_mem_gpu=0
+
+while ps --pid $pid &>/dev/null
+do
+    mem=$(ps -p $pid -o pmem=)
+    mem_gpu=$(gpustat -p | grep $pid | cut -d '|' -f3 | cut -d '/' -f1)
+    if (($(echo "$mem > $max_mem" | bc -l)))
+    then
+        max_mem=$mem
+    fi
+    if [[ $mem_gpu -gt $max_mem_gpu ]]
+    then
+        max_mem_gpu=$mem_gpu
+    fi
+done
+
+echo "Maximum (resil
